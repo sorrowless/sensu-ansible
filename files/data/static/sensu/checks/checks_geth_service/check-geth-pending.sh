590b0e68914ab9894b1cd15b925eda415820389f
sorrowless <stabog.tmb@gmail.com>
2019-07-31 11:29:23 +0300

    Ensure geth pending check doesn't show stderr in out

--- files/data/static/sensu/checks/checks_geth_service/check-geth-pending.sh
+++ files/data/static/sensu/checks/checks_geth_service/check-geth-pending.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PORT="${1:-9656}"
-OUT=`geth attach http://localhost:${PORT} --exec "eth.pendingTransactions.length" 2>&1`
+OUT=`geth attach http://localhost:${PORT} --exec "eth.pendingTransactions.length" 2>/dev/null`
 RC=$?
 
 if [[ $RC -eq 0 ]]; then
